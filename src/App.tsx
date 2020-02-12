e78d3a29f161d0079aa2372cb507e2fb1cf12b64
Evan Bailey <baileyevang@gmail.com>
2020-02-11 21:32:03 -0800

    Add a Hello World card

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,28 @@ import "./App.css";
 
 import React from "react";
 import { TopBar } from "./TopBar";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Container,
+  Box,
+  Grid
+} from "@material-ui/core";
 
 const App = () => {
-  return <TopBar />;
+  return (
+    <>
+      <TopBar />
+      <Container>
+        <Card >
+          <CardContent>
+            <Typography>Hello World</Typography>
+          </CardContent>
+        </Card>
+      </Container>
+    </>
+  );
 };
 
 export default App;
