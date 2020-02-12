0726f1bd430786c86a2061b23d802ea9bb240fdc
Evan Bailey <baileyevang@gmail.com>
2020-02-11 20:35:40 -0800

    Add FontAwesome

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Button from "@material-ui/core/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 import "./App.css";
 import Typography from "@material-ui/core/Typography";
@@ -12,7 +14,7 @@ const App = () => {
     <AppBar position="static">
       <Toolbar>
         <IconButton edge="start" color="inherit" aria-label="menu">
-          Menu
+          <FontAwesomeIcon icon={faBars} />
         </IconButton>
         <Typography variant="h6" style={{ flexGrow: 1 }}>
           Paragon Campaign Dashboard
