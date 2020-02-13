29437af48f103f6ae852858fdee458f1749f9e85
Evan Bailey <baileyevang@gmail.com>
2020-02-12 23:36:43 -0800

    Draggable card

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
 import "./App.css";
 
 import React from "react";
-import { TopBar } from "./TopBar";
 import {
   Card,
   CardContent,
   Typography,
-  Container
+  Container,
+  Box
 } from "@material-ui/core";
+import GridLayout from "react-grid-layout";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+import { TopBar } from "./TopBar";
 
 const App = () => {
   return (
     <>
-      <TopBar />
       <Container>
-        <HelloCard />
+        <TopBar />
+        <GridLayout
+          cols={12}
+          rowHeight={30}
+          width={1200}
+          draggableHandle=".drag-handle"
+          verticalCompact={false}
+          style={{ position: "absolute" }}
+        >
+          <div key="justadiv" data-grid={{ x: 8, y: 0, w: 2, h: 2 }}>
+            <HelloCard />
+          </div>
+        </GridLayout>
       </Container>
     </>
   );
@@ -23,6 +39,7 @@ const App = () => {
 const HelloCard = () => (
   <Card>
     <CardContent>
+      <FontAwesomeIcon icon={faBars} className="drag-handle" />
       <Typography>Hello World</Typography>
     </CardContent>
   </Card>
