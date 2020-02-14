05beafc0bee05b0f12544a8ba3164c1aa7deb166
Evan Bailey <baileyevang@gmail.com>
2020-02-13 20:21:21 -0800

    change key, remove outer fragment

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import "./App.css";
 
 import React from "react";
-import {
-  Card,
-  CardContent,
-  Typography,
-  Container,
-  Box
-} from "@material-ui/core";
+import { Card, CardContent, Typography, Container } from "@material-ui/core";
 import GridLayout from "react-grid-layout";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
@@ -16,23 +10,21 @@ import { TopBar } from "./TopBar";
 
 const App = () => {
   return (
-    <>
-      <Container>
-        <TopBar />
-        <GridLayout
-          cols={12}
-          rowHeight={30}
-          width={1200}
-          draggableHandle=".drag-handle"
-          verticalCompact={false}
-          style={{ position: "absolute" }}
-        >
-          <div key="justadiv" data-grid={{ x: 8, y: 0, w: 2, h: 2 }}>
-            <HelloCard />
-          </div>
-        </GridLayout>
-      </Container>
-    </>
+    <Container>
+      <TopBar />
+      <GridLayout
+        cols={12}
+        rowHeight={30}
+        width={1200}
+        draggableHandle=".drag-handle"
+        verticalCompact={false}
+        style={{ position: "absolute" }}
+      >
+        <div key="hello" data-grid={{ x: 5, y: 0, w: 2, h: 4 }}>
+          <HelloCard />
+        </div>
+      </GridLayout>
+    </Container>
   );
 };
 
