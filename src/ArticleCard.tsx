1ec82e4b14cf9896cd5633f2ec4b4f394b096269
Evan Bailey <baileyevang@gmail.com>
2020-03-16 22:38:08 -0700

    Toggle between edit and view mode

--- src/ArticleCard.tsx
+++ src/ArticleCard.tsx
@@ -2,9 +2,11 @@ import "./ArticleCard.css";
 
 import React from "react";
 import { BaseCard } from "./BaseCard";
-import { Typography, TextField } from "@material-ui/core";
+import { Typography, TextField, IconButton } from "@material-ui/core";
 import { ReducerContext } from "./ReducerContext";
 import { Actions } from "./Actions";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faCheck } from "@fortawesome/free-solid-svg-icons";
 
 export function ArticleCard(props: { cardId: string }) {
   const { state, dispatch } = React.useContext(ReducerContext);
@@ -16,25 +18,47 @@ export function ArticleCard(props: { cardId: string }) {
     }
   };
 
+  const [editMode, setEditMode] = React.useState<boolean>(true);
+
   return (
-    <BaseCard header={<Typography>Article</Typography>}>
-      <TextField
-        className="article-card__text-field"
-        multiline
-        variant="outlined"
-        inputProps={fullHeightStyle}
-        style={{ height: "100%", display: "flex", flexDirection: "column" }}
-        value={cardState.content}
-        onChange={changeEvent => {
-          const content = changeEvent.target.value;
-          dispatch(
-            Actions.SetCardContent({
-              cardId: props.cardId,
-              content
-            })
-          );
-        }}
-      />
+    <BaseCard
+      header={
+        <>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="toggle-edit-mode"
+            onClick={() => setEditMode(!editMode)}
+          >
+            <FontAwesomeIcon icon={editMode ? faCheck : faEdit} />
+          </IconButton>
+          <Typography>Article</Typography>
+        </>
+      }
+    >
+      {editMode ? (
+        <TextField
+          className="article-card__text-field"
+          multiline
+          variant="outlined"
+          inputProps={fullHeightStyle}
+          style={{ height: "100%", display: "flex", flexDirection: "column" }}
+          value={cardState.content}
+          onChange={changeEvent => {
+            const content = changeEvent.target.value;
+            dispatch(
+              Actions.SetCardContent({
+                cardId: props.cardId,
+                content
+              })
+            );
+          }}
+        />
+      ) : (
+        <Typography style={{ whiteSpace: "pre" }}>
+          {cardState.content}
+        </Typography>
+      )}
     </BaseCard>
   );
 }
