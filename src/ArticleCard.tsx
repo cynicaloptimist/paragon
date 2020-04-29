e43d44d9d88971ad4a843939d920626e730331e2
Evan Bailey <baileyevang@gmail.com>
2020-04-28 20:52:47 -0700

    use grommet components

--- src/ArticleCard.tsx
+++ src/ArticleCard.tsx
@@ -2,7 +2,7 @@ import "./ArticleCard.css";
 
 import React from "react";
 import { BaseCard } from "./BaseCard";
-import { Typography, TextField, IconButton } from "@material-ui/core";
+import { Button, Text, TextArea } from "grommet";
 import { ReducerContext } from "./ReducerContext";
 import { Actions } from "./Actions";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -12,52 +12,39 @@ export function ArticleCard(props: { cardId: string }) {
   const { state, dispatch } = React.useContext(ReducerContext);
   const cardState = state.cardsById[props.cardId];
 
-  const fullHeightStyle = {
-    style: {
-      height: "100%"
-    }
-  };
-
   const [editMode, setEditMode] = React.useState<boolean>(true);
 
   return (
     <BaseCard
       header={
         <>
-          <Typography style={{ flexGrow: 1 }}>Article</Typography>
-          <IconButton
-            edge="end"
-            color="inherit"
+          <Text style={{ flexGrow: 1 }}>Article</Text>
+          <Button
             aria-label="toggle-edit-mode"
             onClick={() => setEditMode(!editMode)}
           >
             <FontAwesomeIcon size="xs" icon={editMode ? faCheck : faEdit} />
-          </IconButton>
+          </Button>
         </>
       }
     >
       {editMode ? (
-        <TextField
+        <TextArea
+          style={{ height: "100%" }}
           className="article-card__text-field"
-          multiline
-          variant="outlined"
-          inputProps={fullHeightStyle}
-          style={{ height: "100%", display: "flex", flexDirection: "column" }}
           value={cardState.content}
-          onChange={changeEvent => {
+          onChange={(changeEvent) => {
             const content = changeEvent.target.value;
             dispatch(
               Actions.SetCardContent({
                 cardId: props.cardId,
-                content
+                content,
               })
             );
           }}
         />
       ) : (
-        <Typography style={{ whiteSpace: "pre" }}>
-          {cardState.content}
-        </Typography>
+        <Text style={{ whiteSpace: "pre" }}>{cardState.content}</Text>
       )}
     </BaseCard>
   );
