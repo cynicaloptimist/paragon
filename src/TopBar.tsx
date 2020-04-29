14ef88e6b6dd370a4679ecf7c3e14fa4b3bc7ac9
Evan Bailey <baileyevang@gmail.com>
2020-04-28 21:57:16 -0700

    Add padding to top bar

--- src/TopBar.tsx
+++ src/TopBar.tsx
@@ -4,7 +4,7 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { Header, Button, Text } from "grommet";
 
 export const TopBar = () => (
-  <Header background="brand">
+  <Header background="brand" pad="small">
     <Button icon={<FontAwesomeIcon size="sm" icon={faBars} />} />
     <Text>Paragon Campaign Dashboard</Text>
     <Button>Help</Button>
