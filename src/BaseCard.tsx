b66386116803df0175e314cbfcc0c504375d3bbf
Evan Bailey <baileyevang@gmail.com>
2020-05-04 23:06:01 -0700

    Move drag handle into dedicated button

--- src/BaseCard.tsx
+++ src/BaseCard.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { Box, Header } from "grommet";
+import { Box, Header, Button } from "grommet";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGripLines } from "@fortawesome/free-solid-svg-icons";
 
 export function BaseCard(props: {
   header: React.ReactNode;
@@ -7,8 +9,12 @@ export function BaseCard(props: {
 }) {
   return (
     <Box fill elevation="medium">
-      <Box className="drag-handle">
+      <Box>
         <Header pad="xsmall" background="brand">
+          <Button
+            className="drag-handle"
+            icon={<FontAwesomeIcon icon={faGripLines} />}
+          />
           {props.header}
         </Header>
       </Box>
