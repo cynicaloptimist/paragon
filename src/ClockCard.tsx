28322753216e592c723cc1c520811ac0d8d8416d
Evan Bailey <baileyevang@gmail.com>
2020-05-05 23:07:20 -0700

    Box-based clock control

--- src/ClockCard.tsx
+++ src/ClockCard.tsx
@@ -3,7 +3,7 @@ import { ReducerContext } from "./ReducerContext";
 import { BaseCard } from "./BaseCard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck, faEdit } from "@fortawesome/free-solid-svg-icons";
-import { Button, RangeInput } from "grommet";
+import { Button, Box } from "grommet";
 import { ClockCardState } from "./CardState";
 import { Actions } from "./Actions";
 
@@ -14,11 +14,11 @@ export function ClockCard(props: { card: ClockCardState }) {
   const [isConfigurable, setConfigurable] = React.useState(false);
 
   const setCardValue = React.useCallback(
-    (event: React.ChangeEvent<any>) =>
+    (value: number) =>
       dispatch(
         Actions.SetClockValue({
           cardId: props.card.cardId,
-          value: event.target.value,
+          value,
         })
       ),
     [props.card.cardId, dispatch]
@@ -43,13 +43,32 @@ export function ClockCard(props: { card: ClockCardState }) {
       {isConfigurable ? (
         "Configure"
       ) : (
-        <RangeInput
-          min={0}
-          max={card.max}
-          value={card.value}
-          onChange={setCardValue}
-        />
+        <Clock value={card.value} max={card.max} onChange={setCardValue} />
       )}
     </BaseCard>
+  );
+}
+
+function Clock(props: {
+  value: number;
+  max: number;
+  onChange: (value: number) => void;
+}) {
+  let segments = [];
+  for (let i = 0; i < props.max; i++) {
+    const color = i < props.value ? "brand" : "neutral-3";
+    segments.push(
+      <Box
+        key={i}
+        flex="grow"
+        background={color}
+        onClick={() => props.onChange(i + 1)}
+      />
+    );
+  }
+  return (
+    <Box direction="row" height="xxsmall" gap="xxsmall" justify="stretch">
+      {segments}
+    </Box>
   );
 }
