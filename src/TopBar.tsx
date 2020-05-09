23e30a9610e2392e7ba95b4bf63c320a1e52c8bb
Evan Bailey <baileyevang@gmail.com>
2020-05-08 22:01:12 -0700

    use heading element

--- src/TopBar.tsx
+++ src/TopBar.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faPlus } from "@fortawesome/free-solid-svg-icons";
-import { Header, Button, Text } from "grommet";
+import { Header, Button, Heading } from "grommet";
 import { ReducerContext } from "./ReducerContext";
 import { Actions } from "./Actions";
 
@@ -10,7 +10,9 @@ export const TopBar = () => {
   return (
     <Header background="brand" pad="small">
       <Button icon={<FontAwesomeIcon size="sm" icon={faBars} />} />
-      <Text>Paragon Campaign Dashboard</Text>
+      <Heading level={1} size="small" margin="xxsmall">
+        Paragon Campaign Dashboard
+      </Heading>
       <Button
         aria-label="add"
         onClick={() => dispatch(Actions.AddCard())}
