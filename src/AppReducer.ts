2af9b9c34bf49d3998e31e70856c3ef4dad57447
Evan Bailey <baileyevang@gmail.com>
2020-06-14 22:44:04 -0700

    Rename and move method to file

--- src/AppReducer.ts
+++ src/AppReducer.ts
@@ -3,8 +3,8 @@ import { omit } from "lodash";
 
 import { RootAction, Actions } from "./Actions";
 import { AppState } from "./AppState";
-import { CardState } from "./CardState";
 import { CardsReducer } from "./CardsReducer";
+import { InitialCardState } from "./InitialCardState";
 
 export function AppReducer(oldState: AppState, action: RootAction) {
   if (isActionOf(Actions.AddCard, action)) {
@@ -14,7 +14,7 @@ export function AppReducer(oldState: AppState, action: RootAction) {
       openCardIds: oldState.openCardIds.concat([cardId]),
       cardsById: {
         ...oldState.cardsById,
-        [cardId]: newCard(cardId, cardType),
+        [cardId]: InitialCardState(cardId, cardType),
       },
     };
   }
@@ -48,49 +48,5 @@ export function AppReducer(oldState: AppState, action: RootAction) {
   return {
     ...oldState,
     cardsById: CardsReducer(oldState.cardsById, action),
-  };
-}
-
-function newCard(cardId: string, type: string): CardState {
-  const baseCard = {
-    cardId,
-    title: "New Card",
-  };
-
-  if (type === "clock") {
-    return {
-      ...baseCard,
-      type,
-      value: 0,
-      max: 6,
-    };
-  }
-
-  if (type === "roll-table") {
-    return {
-      ...baseCard,
-      type,
-      entries: [
-        { weight: 1, content: "Test" },
-        { weight: 1, content: "Test2" },
-        { weight: 2, content: "Test3" },
-        { weight: 2, content: "Test4" },
-        { weight: 1, content: "Test5" },
-      ],
-    };
-  }
-
-  if (type === "image") {
-    return {
-      ...baseCard,
-      type,
-      imageUrl: "",
-    };
-  }
-
-  return {
-    ...baseCard,
-    type: "article",
-    content: "",
   };
 }
---
+++ src/InitialCardState.ts
@@ -0,0 +1,45 @@
+import { CardState } from "./CardState";
+
+export function InitialCardState(cardId: string, type: string): CardState {
+  const baseCard = {
+    cardId,
+    title: "New Card",
+  };
+
+  if (type === "clock") {
+    return {
+      ...baseCard,
+      type,
+      value: 0,
+      max: 6,
+    };
+  }
+
+  if (type === "roll-table") {
+    return {
+      ...baseCard,
+      type,
+      entries: [
+        { weight: 1, content: "Test" },
+        { weight: 1, content: "Test2" },
+        { weight: 2, content: "Test3" },
+        { weight: 2, content: "Test4" },
+        { weight: 1, content: "Test5" },
+      ],
+    };
+  }
+
+  if (type === "image") {
+    return {
+      ...baseCard,
+      type,
+      imageUrl: "",
+    };
+  }
+
+  return {
+    ...baseCard,
+    type: "article",
+    content: "",
+  };
+}
