af60ed90ff94cbeb0731f2cd1f353e2575c834a0
Evan Bailey <baileyevang@gmail.com>
2020-06-14 23:28:40 -0700

    Add firebase initializeApp

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB_rK2M4U6Jff9f1tZwPmH7rkrFYQ4nX8k",
+  authDomain: "paragon-25f54.firebaseapp.com",
+  databaseURL: "https://paragon-25f54.firebaseio.com",
+  projectId: "paragon-25f54",
+  storageBucket: "paragon-25f54.appspot.com",
+  messagingSenderId: "588974354898",
+  appId: "1:588974354898:web:5bda5386460d411e40580b",
+  measurementId: "G-8YH019LRF3"
+};
--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+
+import { initializeApp } from "firebase/app";
+import "firebase/app";
+import { firebaseConfig } from "./firebaseConfig";
+
 import { PreventDefaultWindowDragDropEvents } from "./PreventDefaultWindowDragDropEvents";
 
+initializeApp(firebaseConfig);
 PreventDefaultWindowDragDropEvents();
 ReactDOM.render(<App />, document.getElementById("root"));
 
