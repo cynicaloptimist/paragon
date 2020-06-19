0cd633839a12ad5e2d5f69916292824d987a0c13
Evan Bailey <baileyevang@gmail.com>
2020-06-18 23:15:48 -0700

    Add initial player view

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,15 @@ import React from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
 import { GameMasterView } from "./GameMasterView";
+import { PlayerView } from "./PlayerView";
 
 const App = () => {
   return (
     <Router>
       <Switch>
+        <Route path="/p">
+          <PlayerView />
+        </Route>
         <Route path="/">
           <GameMasterView />
         </Route>
---
+++ src/PlayerView.tsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+
+import { database } from "firebase/app";
+import "firebase/database";
+
+import { ReducerContext } from "./ReducerContext";
+import { Grommet, Box } from "grommet";
+import { Theme } from "./Theme";
+import { TopBar } from "./TopBar";
+import { CardGrid } from "./CardGrid";
+import { GetInitialState, AppState } from "./AppState";
+
+function useRemoteState() {
+  const [state, setState] = useState(GetInitialState());
+  useEffect(() => {
+    const dbRef = database().ref("playerviews/test");
+    dbRef.on("value", (appState) => {
+      const networkGameState: AppState = appState.val().state;
+      setState(networkGameState);
+    });
+
+    return () => dbRef.off();
+  }, []);
+  return state;
+}
+
+export function PlayerView() {
+  const state = useRemoteState();
+
+  return (
+    <ReducerContext.Provider value={{ state, dispatch: () => {} }}>
+      <Grommet style={{ height: "100%" }} theme={Theme}>
+        <Box fill align="center">
+          <Box fill="vertical" width="1200px">
+            <TopBar />
+            <CardGrid />
+          </Box>
+        </Box>
+      </Grommet>
+    </ReducerContext.Provider>
+  );
+}
