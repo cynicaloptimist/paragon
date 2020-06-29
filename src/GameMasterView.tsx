c3c7c8f1aa100d86a492d448b533908bacf9ee7a
Evan Bailey <baileyevang@gmail.com>
2020-06-28 21:43:56 -0700

    Add header for sidebar

--- src/GameMasterView.tsx
+++ src/GameMasterView.tsx
@@ -4,12 +4,14 @@ import { TopBar } from "./TopBar";
 import { AppReducer } from "./AppReducer";
 import { GetInitialState } from "./AppState";
 import { ReducerContext } from "./ReducerContext";
-import { Grommet, Box, Text } from "grommet";
+import { Grommet, Box, Text, Header, Button } from "grommet";
 import { useStorageBackedReducer } from "./useStorageBackedReducer";
 import { CardGrid } from "./CardGrid";
 import { Theme } from "./Theme";
 import { useServerStateUpdates } from "./useServerStateUpdates";
 import values from "lodash/values";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export function GameMasterView() {
   const [state, dispatch] = useStorageBackedReducer(
@@ -43,9 +45,14 @@ function CardLibrary() {
       elevation="large"
       style={{ position: "fixed", width: "300px", height: "100%" }}
     >
-      {values(state.cardsById).map((card) => (
-        <Text key={card.cardId}>{card.title}</Text>
-      ))}
+      <Header background="brand" pad="small">
+        <Button icon={<FontAwesomeIcon size="sm" icon={faBars} />} />
+      </Header>
+      <Box pad="xsmall">
+        {values(state.cardsById).map((card) => (
+          <Text key={card.cardId}>{card.title}</Text>
+        ))}
+      </Box>
     </Box>
   );
 }
