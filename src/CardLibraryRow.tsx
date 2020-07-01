f4ce61a39369ac380509a5644057a692181c3c98
Evan Bailey <baileyevang@gmail.com>
2020-06-30 23:09:09 -0700

    Add delete button

--- src/CardLibraryRow.tsx
+++ src/CardLibraryRow.tsx
@@ -1,10 +1,12 @@
 import React, { useContext, useCallback } from "react";
 import { ReducerContext } from "./ReducerContext";
-import { Button } from "grommet";
+import { Button, Box } from "grommet";
 import { Actions } from "./Actions";
 import { CardState } from "./CardState";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-export function CardLibraryRow(props: { card: CardState; }) {
+export function CardLibraryRow(props: { card: CardState }) {
   const { dispatch } = useContext(ReducerContext);
 
   const openCard = useCallback(
@@ -12,9 +14,17 @@ export function CardLibraryRow(props: { card: CardState; }) {
     [dispatch, props.card.cardId]
   );
 
+  const deleteCard = useCallback(
+    () => dispatch(Actions.DeleteCard({ cardId: props.card.cardId })),
+    [dispatch, props.card.cardId]
+  );
+
   return (
-    <Button key={props.card.cardId} onClick={openCard}>
-      {props.card.title}
-    </Button>
+    <Box key={props.card.cardId} direction="row">
+      <Button onClick={openCard} fill="horizontal">
+        {props.card.title}
+      </Button>
+      <Button onClick={deleteCard} icon={<FontAwesomeIcon icon={faTrash} />} />
+    </Box>
   );
 }
