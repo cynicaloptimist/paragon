6f3de612291950d290579cf1fc1ee31e14a2f41b
Evan Bailey <baileyevang@gmail.com>
2020-08-08 22:38:40 -0700

    Toggle between type and folder groupings

--- src/components/CardLibrary.tsx
+++ src/components/CardLibrary.tsx
@@ -1,46 +1,61 @@
-import React, { useContext, useCallback } from "react";
+import React, { useContext, useCallback, useState } from "react";
 import { ReducerContext } from "../reducers/ReducerContext";
 import { Box, Header, Button, Heading } from "grommet";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faSort } from "@fortawesome/free-solid-svg-icons";
 import { Actions } from "../actions/Actions";
 import { CardLibraryRow } from "./CardLibraryRow";
 import { CardState } from "../state/CardState";
 import { CardTypeFriendlyNames } from "../state/CardTypeFriendlyNames";
 
+enum Grouping {
+  ByType,
+  ByFolder,
+}
+
+const GroupingFunctions: Record<Grouping, (cardState: CardState) => string> = {
+  [Grouping.ByType]: (cardState: CardState) =>
+    CardTypeFriendlyNames[cardState.type],
+  [Grouping.ByFolder]: (cardState: CardState) => cardState.path ?? "",
+};
+
 export function CardLibrary() {
   const { state, dispatch } = useContext(ReducerContext);
+  const [grouping, setGrouping] = useState(Grouping.ByType);
 
   const hideCardLibrary = useCallback(
     () => dispatch(Actions.SetCardLibraryVisibility({ visibility: false })),
     [dispatch]
   );
 
-  const cardsByType = Object.values(state.cardsById).reduce(
+  const cardsByGroup = Object.values(state.cardsById).reduce(
     (hash: Record<string, CardState[]>, cardState) => {
-      if (hash[cardState.type] === undefined) {
-        hash[cardState.type] = [];
+      const cardGroup = GroupingFunctions[grouping](cardState);
+      if (hash[cardGroup] === undefined) {
+        hash[cardGroup] = [];
       }
 
-      hash[cardState.type].push(cardState);
+      hash[cardGroup].push(cardState);
 
       return hash;
     },
     {}
   );
 
-  const headersAndCards = Object.keys(cardsByType).map((cardType) => {
-    return (
-      <Box flex={false}>
-        <Heading level={3} margin="xsmall">
-          {CardTypeFriendlyNames[cardType]}
-        </Heading>
-        {cardsByType[cardType].map((card) => (
-          <CardLibraryRow key={card.cardId} card={card} />
-        ))}
-      </Box>
-    );
-  });
+  const headersAndCards = Object.keys(cardsByGroup)
+    .sort()
+    .map((cardGroup) => {
+      return (
+        <Box flex={false} key={cardGroup}>
+          <Heading level={3} margin="xsmall">
+            {cardGroup}
+          </Heading>
+          {cardsByGroup[cardGroup].map((card) => (
+            <CardLibraryRow key={card.cardId} card={card} />
+          ))}
+        </Box>
+      );
+    });
 
   return (
     <Box
@@ -52,6 +67,14 @@ export function CardLibrary() {
         <Button
           icon={<FontAwesomeIcon size="sm" icon={faBars} />}
           onClick={hideCardLibrary}
+        />
+        <Button
+          icon={<FontAwesomeIcon size="sm" icon={faSort} />}
+          onClick={() => {
+            const nextGrouping =
+              (grouping + 1) % (Object.keys(Grouping).length / 2);
+            setGrouping(nextGrouping);
+          }}
         />
       </Header>
       <Box pad="xsmall" overflow={{ vertical: "auto" }}>
