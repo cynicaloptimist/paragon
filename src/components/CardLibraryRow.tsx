01dd033d6505f006908dedaf1615568f4490cc57
Evan Bailey <baileyevang@gmail.com>
2020-08-08 22:27:47 -0700

    Add inline path editor

--- src/components/CardLibraryRow.tsx
+++ src/components/CardLibraryRow.tsx
@@ -1,10 +1,10 @@
-import React, { useContext, useCallback } from "react";
+import React, { useContext, useCallback, useState } from "react";
 import { ReducerContext } from "../reducers/ReducerContext";
-import { Button, Box } from "grommet";
+import { Button, Box, TextInput } from "grommet";
 import { CardActions } from "../actions/Actions";
 import { CardState } from "../state/CardState";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faFolder, faCheck } from "@fortawesome/free-solid-svg-icons";
 
 export function CardLibraryRow(props: { card: CardState }) {
   const { dispatch } = useContext(ReducerContext);
@@ -19,11 +19,47 @@ export function CardLibraryRow(props: { card: CardState }) {
     [dispatch, props.card.cardId]
   );
 
+  const [editingPath, setEditingPath] = useState(false);
+  const [pathInput, setPathInput] = useState("");
+  const saveAndClose = useCallback(() => {
+    dispatch(
+      CardActions.SetCardPath({ cardId: props.card.cardId, path: pathInput })
+    );
+    setEditingPath(false);
+    setPathInput("");
+  }, [dispatch, props.card.cardId, pathInput, setPathInput, setEditingPath]);
+
+  if (editingPath) {
+    return (
+      <Box flex={false} key={props.card.cardId} direction="row">
+        <TextInput
+          placeholder={props.card.path}
+          onChange={(changeEvent) => setPathInput(changeEvent.target.value)}
+          onKeyDown={(keyEvent) => {
+            if (keyEvent.key === "Enter") {
+              saveAndClose();
+            }
+          }}
+          autoFocus
+          onBlur={saveAndClose}
+        />
+        <Button
+          onClick={() => setEditingPath(false)}
+          icon={<FontAwesomeIcon icon={faCheck} />}
+        />
+      </Box>
+    );
+  }
+
   return (
     <Box flex={false} key={props.card.cardId} direction="row">
       <Button onClick={openCard} fill="horizontal">
         {props.card.title}
       </Button>
+      <Button
+        onClick={() => setEditingPath(true)}
+        icon={<FontAwesomeIcon icon={faFolder} />}
+      />
       <Button onClick={deleteCard} icon={<FontAwesomeIcon icon={faTrash} />} />
     </Box>
   );
