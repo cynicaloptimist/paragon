ae5c3d399a583e8a3531dbcc93dc842b4c0cf04c
Evan Bailey <cynicaloptimist@gmail.com>
2020-10-02 23:06:53 -0700

    Add trivial test for legacy state updates

---
+++ src/state/UpdateMissingOrLegacyAppState.test.tsx
@@ -0,0 +1,19 @@
+import { EmptyState } from "./AppState";
+import {
+  LegacyAppState,
+  UpdateMissingOrLegacyAppState
+} from "./LegacyAppState";
+
+test("updates legacy stored state", () => {
+  const storedState: LegacyAppState = {
+    openCardIds: [],
+    cardsById: {},
+    layouts: [],
+    cardLibraryVisibility: false,
+    layoutCompaction: "free",
+    playerViewId: "",
+  };
+  const updatedState = UpdateMissingOrLegacyAppState(storedState);
+
+  expect(updatedState).toEqual(EmptyState());
+});
