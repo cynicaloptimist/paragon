eae9aaa2fd465576bbfcc1aa94571e94d0a0e2f5
Evan Bailey <cynicaloptimist@gmail.com>
2020-10-16 21:34:38 -0700

    Move component to file

--- src/components/CardLibraryRow.tsx
+++ src/components/CardLibraryRow.tsx
@@ -1,10 +1,11 @@
 import { faCheck, faFolder, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Box, Button, ButtonType, Meter, Stack, TextInput } from "grommet";
-import React, { useCallback, useContext, useRef, useState } from "react";
+import { Box, Button, TextInput } from "grommet";
+import React, { useCallback, useContext, useState } from "react";
 import { CardActions } from "../actions/Actions";
 import { ReducerContext } from "../reducers/ReducerContext";
 import { CardState } from "../state/CardState";
+import { LongPressButton } from "./LongPressButton";
 
 export function CardLibraryRow(props: { card: CardState }) {
   const { dispatch } = useContext(ReducerContext);
@@ -64,72 +65,5 @@ export function CardLibraryRow(props: { card: CardState }) {
         icon={<FontAwesomeIcon icon={faTrash} />}
       />
     </Box>
-  );
-}
-
-const DRAW_INTERVAL = 20;
-
-function LongPressButton(
-  props: Omit<ButtonType, "onClick"> & {
-    timeout?: number;
-    onLongPress: () => void;
-  }
-) {
-  const { onLongPress, ...buttonProps } = props;
-  const timeout = props.timeout || 1000;
-
-  const [pressLength, setPressLength] = useState(0);
-  const pressLengthRef = useRef(pressLength);
-  pressLengthRef.current = pressLength;
-
-  const interval = useRef<NodeJS.Timeout>();
-
-  const press = useCallback(() => {
-    if (!interval.current) {
-      interval.current = setInterval(() => {
-        if (pressLengthRef.current > timeout) {
-          onLongPress();
-        } else {
-          setPressLength(pressLengthRef.current + DRAW_INTERVAL);
-        }
-      }, DRAW_INTERVAL);
-    }
-  }, [onLongPress, timeout]);
-
-  const unPress = useCallback(() => {
-    if (interval.current) {
-      clearInterval(interval.current);
-      interval.current = undefined;
-    }
-    setPressLength(0);
-  }, [interval, setPressLength]);
-
-  return (
-    <Stack anchor="center">
-      <Meter
-        type="circle"
-        max={timeout}
-        values={[
-          {
-            value: pressLength,
-            color: "status-warning",
-          },
-        ]}
-        size="xxsmall"
-        thickness="xsmall"
-        margin={{
-          top: "5px",
-        }}
-      />
-      <Button
-        {...buttonProps}
-        onMouseDown={press}
-        onMouseUp={unPress}
-        onMouseLeave={unPress}
-        onTouchStart={press}
-        onTouchEnd={unPress}
-        onTouchCancel={unPress}
-      />
-    </Stack>
   );
 }
---
+++ src/components/LongPressButton.tsx
@@ -0,0 +1,69 @@
+import { Button, ButtonType, Meter, Stack } from "grommet";
+import React, { useCallback, useRef, useState } from "react";
+
+const DRAW_INTERVAL = 20;
+
+export function LongPressButton(
+  props: Omit<ButtonType, "onClick"> & {
+    timeout?: number;
+    onLongPress: () => void;
+  }
+) {
+  const { onLongPress, ...buttonProps } = props;
+  const timeout = props.timeout || 1000;
+
+  const [pressLength, setPressLength] = useState(0);
+  const pressLengthRef = useRef(pressLength);
+  pressLengthRef.current = pressLength;
+
+  const interval = useRef<NodeJS.Timeout>();
+
+  const press = useCallback(() => {
+    if (!interval.current) {
+      interval.current = setInterval(() => {
+        if (pressLengthRef.current > timeout) {
+          onLongPress();
+        } else {
+          setPressLength(pressLengthRef.current + DRAW_INTERVAL);
+        }
+      }, DRAW_INTERVAL);
+    }
+  }, [onLongPress, timeout]);
+
+  const unPress = useCallback(() => {
+    if (interval.current) {
+      clearInterval(interval.current);
+      interval.current = undefined;
+    }
+    setPressLength(0);
+  }, [interval, setPressLength]);
+
+  return (
+    <Stack anchor="center">
+      <Meter
+        type="circle"
+        max={timeout}
+        values={[
+          {
+            value: pressLength,
+            color: "status-warning",
+          },
+        ]}
+        size="xxsmall"
+        thickness="xsmall"
+        margin={{
+          top: "5px",
+        }}
+      />
+      <Button
+        {...buttonProps}
+        onMouseDown={press}
+        onMouseUp={unPress}
+        onMouseLeave={unPress}
+        onTouchStart={press}
+        onTouchEnd={unPress}
+        onTouchCancel={unPress}
+      />
+    </Stack>
+  );
+}
