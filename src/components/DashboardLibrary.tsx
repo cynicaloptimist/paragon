c7c6451970d7f4babb40da4757e1d5169fa8c1cd
Evan Bailey <cynicaloptimist@gmail.com>
2020-10-16 21:42:57 -0700

    Extract dashboardlibraryrow, add trash button

--- src/components/DashboardLibrary.tsx
+++ src/components/DashboardLibrary.tsx
@@ -1,10 +1,12 @@
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Box, Button } from "grommet";
-import React, { useContext } from "react";
+import React, { useCallback, useContext } from "react";
 import { Actions } from "../actions/Actions";
 import { randomString } from "../randomString";
 import { ReducerContext } from "../reducers/ReducerContext";
+import { DashboardState } from "../state/AppState";
+import { LongPressButton } from "./LongPressButton";
 
 export function DashboardLibrary() {
   const { state, dispatch } = useContext(ReducerContext);
@@ -13,15 +15,7 @@ export function DashboardLibrary() {
       {Object.keys(state.dashboardsById).map((dashboardId) => {
         const dashboard = state.dashboardsById[dashboardId];
         return (
-          <Box flex={false} direction="row">
-            <Button
-              onClick={() =>
-                dispatch(Actions.ActivateDashboard({ dashboardId }))
-              }
-            >
-              {dashboard.name}
-            </Button>
-          </Box>
+          <DashboardLibraryRow dashboardId={dashboardId} dashboard={dashboard} />
         );
       })}
       <Button
@@ -31,6 +25,35 @@ export function DashboardLibrary() {
         fill="horizontal"
         label="New Dashboard"
         icon={<FontAwesomeIcon size="sm" icon={faPlus} />}
+      />
+    </Box>
+  );
+}
+
+export function DashboardLibraryRow(props: {
+  dashboardId: string;
+  dashboard: DashboardState;
+}) {
+  const { dispatch } = useContext(ReducerContext);
+
+  const openCard = useCallback(
+    () =>
+      dispatch(Actions.ActivateDashboard({ dashboardId: props.dashboardId })),
+    [dispatch, props.dashboardId]
+  );
+
+  const deleteCard = useCallback(() => {
+    dispatch(Actions.ActivateDashboard({ dashboardId: props.dashboardId }));
+  }, [dispatch, props.dashboardId]);
+
+  return (
+    <Box flex={false} direction="row">
+      <Button onClick={openCard} fill="horizontal">
+        {props.dashboard.name}
+      </Button>
+      <LongPressButton
+        onLongPress={deleteCard}
+        icon={<FontAwesomeIcon icon={faTrash} />}
       />
     </Box>
   );
