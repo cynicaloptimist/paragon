b9287c6d25ca6539f60ed7f627ca1cf51389b48f
Evan Bailey <cynicaloptimist@gmail.com>
2020-10-23 13:09:30 -0700

    move component to file

--- src/components/DashboardLibrary.tsx
+++ src/components/DashboardLibrary.tsx
@@ -1,12 +1,11 @@
-import { faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Box, Button } from "grommet";
-import React, { useCallback, useContext } from "react";
+import React, { useContext } from "react";
 import { Actions } from "../actions/Actions";
 import { randomString } from "../randomString";
 import { ReducerContext } from "../reducers/ReducerContext";
-import { DashboardState } from "../state/AppState";
-import { LongPressButton } from "./LongPressButton";
+import { DashboardLibraryRow } from "./DashboardLibraryRow";
 
 export function DashboardLibrary() {
   const { state, dispatch } = useContext(ReducerContext);
@@ -15,7 +14,10 @@ export function DashboardLibrary() {
       {Object.keys(state.dashboardsById).map((dashboardId) => {
         const dashboard = state.dashboardsById[dashboardId];
         return (
-          <DashboardLibraryRow dashboardId={dashboardId} dashboard={dashboard} />
+          <DashboardLibraryRow
+            dashboardId={dashboardId}
+            dashboard={dashboard}
+          />
         );
       })}
       <Button
@@ -25,35 +27,6 @@ export function DashboardLibrary() {
         fill="horizontal"
         label="New Dashboard"
         icon={<FontAwesomeIcon size="sm" icon={faPlus} />}
-      />
-    </Box>
-  );
-}
-
-export function DashboardLibraryRow(props: {
-  dashboardId: string;
-  dashboard: DashboardState;
-}) {
-  const { dispatch } = useContext(ReducerContext);
-
-  const openDashboard = useCallback(
-    () =>
-      dispatch(Actions.ActivateDashboard({ dashboardId: props.dashboardId })),
-    [dispatch, props.dashboardId]
-  );
-
-  const deleteDashboard = useCallback(() => {
-    dispatch(Actions.DeleteDashboard({ dashboardId: props.dashboardId }));
-  }, [dispatch, props.dashboardId]);
-
-  return (
-    <Box flex={false} direction="row">
-      <Button onClick={openDashboard} fill="horizontal">
-        {props.dashboard.name}
-      </Button>
-      <LongPressButton
-        onLongPress={deleteDashboard}
-        icon={<FontAwesomeIcon icon={faTrash} />}
       />
     </Box>
   );
---
+++ src/components/DashboardLibraryRow.tsx
@@ -0,0 +1,37 @@
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Box, Button } from "grommet";
+import React, { useCallback, useContext } from "react";
+import { Actions } from "../actions/Actions";
+import { ReducerContext } from "../reducers/ReducerContext";
+import { DashboardState } from "../state/AppState";
+import { LongPressButton } from "./LongPressButton";
+
+export function DashboardLibraryRow(props: {
+  dashboardId: string;
+  dashboard: DashboardState;
+}) {
+  const { state, dispatch } = useContext(ReducerContext);
+
+  const openDashboard = useCallback(
+    () =>
+      dispatch(Actions.ActivateDashboard({ dashboardId: props.dashboardId })),
+    [dispatch, props.dashboardId]
+  );
+
+  const deleteDashboard = useCallback(() => {
+    dispatch(Actions.DeleteDashboard({ dashboardId: props.dashboardId }));
+  }, [dispatch, props.dashboardId]);
+
+  return (
+    <Box flex={false} direction="row">
+      <Button onClick={openDashboard} fill="horizontal">
+        {props.dashboard.name}
+      </Button>
+      <LongPressButton
+        onLongPress={deleteDashboard}
+        icon={<FontAwesomeIcon icon={faTrash} />}
+      />
+    </Box>
+  );
+}
