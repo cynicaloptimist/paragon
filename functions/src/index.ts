e6ce33f8c335c473958776eeb0008e25b986bc89
Evan Bailey <cynicaloptimist@gmail.com>
2020-12-02 14:04:39 -0800

    Call Patreon API after Oauth redirect

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,16 @@
 import * as functions from "firebase-functions";
+import * as Url from "url";
+const patreon = require("patreon");
+
+const { patreon_config } = functions.config();
+
+const patreonAPI = patreon.patreon;
+const patreonOAuth = patreon.oauth;
+
+const patreonOAuthClient = patreonOAuth(
+  patreon_config.client_id,
+  patreon_config.client_secret
+);
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -16,5 +28,28 @@ export const patreon_login = functions.https.onRequest((request, response) => {
   }
 
   functions.logger.info("Patreon Login Redirect: ", request.query);
-  response.sendStatus(200);
+
+  const oauthGrantCode = Url.parse(request.url, true).query.code;
+
+  patreonOAuthClient
+    .getTokens(oauthGrantCode, patreon_config.redirect_url)
+    .then(function (tokensResponse: { access_token: string }) {
+      const patreonAPIClient = patreonAPI(tokensResponse.access_token);
+      return patreonAPIClient("/current_user");
+    })
+    .then(function (result: { store: any }) {
+      const store = result.store;
+      // store is a [JsonApiDataStore](https://github.com/beauby/jsonapi-datastore)
+      // You can also ask for result.rawJson if you'd like to work with unparsed data
+      const users = store.findAll("user").map((user: any) => user.serialize());
+      functions.logger.info("Outputting users:");
+      functions.logger.info(users);
+      functions.logger.info("Done.");
+
+      response.sendStatus(200);
+    })
+    .catch(function (err: string) {
+      functions.logger.error("error!", err);
+      response.status(500).send(err);
+    });
 });
