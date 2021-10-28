09a94890e986e68a76a9942735c1b57a08d7c2be
Evan Bailey <cynicaloptimist@gmail.com>
2021-10-28 00:24:30 -0700

    add react-sketch drawing canvas

--- src/components/DrawingCard.tsx
+++ src/components/DrawingCard.tsx
@@ -1,12 +1,33 @@
-import React from "react";
+import { faMousePointer, faPen } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button } from "grommet";
+import React, { useState } from "react";
 import { DrawingCardState } from "../state/CardState";
 import { BaseCard } from "./BaseCard";
 
+const { SketchField, Tools } = require("react-sketch2");
 
 export function DrawingCard(props: { card: DrawingCardState }) {
+  const [tool, setTool] = useState(Tools.Pencil);
   return (
-    <BaseCard cardState={props.card} commands={null}>
-      Drawing Card
+    <BaseCard
+      cardState={props.card}
+      commands={
+        <>
+          <Button
+            onClick={() => setTool(Tools.Select)}
+            icon={<FontAwesomeIcon icon={faMousePointer} />}
+            active={tool === Tools.Select}
+          />
+          <Button
+            onClick={() => setTool(Tools.Pencil)}
+            icon={<FontAwesomeIcon icon={faPen} />}
+            active={tool === Tools.Pencil}
+          />
+        </>
+      }
+    >
+      <SketchField tool={tool} />
     </BaseCard>
   );
 }
