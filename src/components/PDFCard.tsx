51c0f8de74b4a764ea5febafaced5bad9356d075
Evan Bailey <cynicaloptimist@gmail.com>
2021-10-30 14:56:54 -0700

    fit height/width buttons

--- src/components/PDFCard.tsx
+++ src/components/PDFCard.tsx
@@ -1,5 +1,7 @@
-import { Box } from "grommet";
-import React from "react";
+import { faArrowsAltH, faArrowsAltV } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Box, Button } from "grommet";
+import React, { useState } from "react";
 import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
 import { CardState } from "../state/CardState";
 import { BaseCard } from "./BaseCard";
@@ -10,11 +12,36 @@ type Size = {
 };
 
 export function PDFCard(props: { card: CardState; outerSize: Size }) {
+  const [fitType, setFitType] = useState("width");
+  const [pageNumber, setPageNumber] = useState(1);
+
   return (
-    <BaseCard cardState={props.card} commands={null}>
+    <BaseCard
+      cardState={props.card}
+      commands={
+        <>
+          <Button
+            icon={<FontAwesomeIcon icon={faArrowsAltV} />}
+            onClick={() => setFitType("height")}
+            active={fitType === "height"}
+          />
+          <Button
+            icon={<FontAwesomeIcon icon={faArrowsAltH} />}
+            onClick={() => setFitType("width")}
+            active={fitType === "width"}
+          />
+        </>
+      }
+    >
       <Box overflow="auto" alignContent="center">
         <Document file="sample.pdf">
-          <Page pageNumber={1} width={props.outerSize.width - 100} />
+          <Page
+            pageNumber={pageNumber}
+            width={fitType === "width" ? props.outerSize.width - 40 : undefined}
+            height={
+              fitType === "height" ? props.outerSize.height - 110 : undefined
+            }
+          />
         </Document>
       </Box>
     </BaseCard>
