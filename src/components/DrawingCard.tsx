0603999dea7b0f5499011ba10cd1660ac2e75492
Evan Bailey <cynicaloptimist@gmail.com>
2021-11-21 21:58:34 -0800

    Add color picker for sketch field

--- src/components/DrawingCard.tsx
+++ src/components/DrawingCard.tsx
@@ -1,9 +1,15 @@
-import { faMousePointer, faPen } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCircle as faCircleSolid,
+  faMousePointer,
+  faPen,
+} from "@fortawesome/free-solid-svg-icons";
 import { faCircle, faSquare } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Box, Button } from "grommet";
+import { Box, Button, Drop } from "grommet";
 import _ from "lodash";
 import React, { useContext, useRef, useState } from "react";
+import { SketchPicker } from "react-color";
+
 import { CardActions } from "../actions/CardActions";
 import { ReducerContext } from "../reducers/ReducerContext";
 import { DrawingCardState, PlayerViewPermission } from "../state/CardState";
@@ -32,6 +38,8 @@ export function DrawingCard(props: {
 }) {
   const { dispatch } = React.useContext(ReducerContext);
   const [activeTool, setActiveTool] = useState<string>(Tools.Pencil);
+  const [color, setColor] = useState<string>("#000000");
+
   const boxRef = React.useRef<HTMLDivElement>(null);
   const [innerSize, setInnerSize] = React.useState<Size>({
     height: 0,
@@ -99,6 +107,7 @@ export function DrawingCard(props: {
       cardState={props.card}
       commands={
         <>
+          <ColorPickerButton color={color} setColor={setColor} />
           {tools.map((tool) => (
             <Button
               key={tool.name}
@@ -124,6 +133,7 @@ export function DrawingCard(props: {
       >
         <SketchField
           tool={canEdit ? activeTool : Tools.Pan}
+          lineColor={color}
           value={sketchModel}
           ref={sketch}
           width={innerSize.width}
@@ -131,5 +141,38 @@ export function DrawingCard(props: {
         />
       </Box>
     </BaseCard>
+  );
+}
+
+function ColorPickerButton(props: {
+  color: string;
+  setColor: (color: string) => void;
+}) {
+  const buttonRef = React.useRef(null);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <>
+      {isOpen && buttonRef.current && (
+        <Drop
+          target={buttonRef.current}
+          align={{ bottom: "top", right: "right" }}
+        >
+          <SketchPicker
+            color={props.color}
+            onChange={(colorResult) => {
+              props.setColor(colorResult.hex);
+            }}
+          />
+        </Drop>
+      )}
+      <Button
+        ref={buttonRef}
+        active={isOpen}
+        key="colorPicker"
+        onClick={() => setIsOpen(!isOpen)}
+        icon={<FontAwesomeIcon color={props.color} icon={faCircleSolid} />}
+      />
+    </>
   );
 }
