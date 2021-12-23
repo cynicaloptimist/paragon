fb2fba126238449dfe16596c5829b6c904d2b0b8
Evan Bailey <cynicaloptimist@gmail.com>
2021-12-22 21:49:50 -0800

    Use layer for modal sidebar

--- src/components/sidebar/LibrarySidebar.tsx
+++ src/components/sidebar/LibrarySidebar.tsx
@@ -1,5 +1,6 @@
-import { Box, Header, Heading } from "grommet";
-import React, { useContext } from "react";
+import { Box, Header, Heading, Layer } from "grommet";
+import React, { useCallback, useContext } from "react";
+import { Actions } from "../../actions/Actions";
 import { ReducerContext } from "../../reducers/ReducerContext";
 import { CardLibrary } from "./CardLibrary";
 import { DashboardLibrary } from "./DashboardLibrary";
@@ -7,31 +8,40 @@ import { LibrarySidebarControls } from "./LibrarySidebarControls";
 import { LoginLogout } from "./LoginLogout";
 
 export function LibrarySidebar() {
-  const { state } = useContext(ReducerContext);
+  const { state, dispatch } = useContext(ReducerContext);
+  const closeSidebar = useCallback(() => {
+    dispatch(Actions.SetLibraryMode({ libraryMode: "hidden" }));
+  }, [dispatch]);
 
   return (
-    <Box
-      background="background"
-      elevation="large"
-      style={{ position: "fixed", left: 0, width: "300px", height: "100%" }}
-      alignContent="center"
+    <Layer
+      onClickOutside={closeSidebar}
+      onEsc={closeSidebar}
+      position="left"
+      full="vertical"
     >
-      <Header background="brand" pad="small" height="xsmall">
-        <LibrarySidebarControls />
-        <Heading level={3} margin="none">
-          {state.librarySidebarMode === "dashboards" ? "Dashboards" : "Cards"}
-        </Heading>
-      </Header>
-      <Box fill>
-        {state.librarySidebarMode === "dashboards" ? (
-          <DashboardLibrary />
-        ) : (
-          <CardLibrary />
-        )}
+      <Box
+        background="background"
+        style={{ width: "300px", minHeight: "100%" }}
+        alignContent="center"
+      >
+        <Header background="brand" pad="small" height="xsmall">
+          <LibrarySidebarControls />
+          <Heading level={3} margin="none">
+            {state.librarySidebarMode === "dashboards" ? "Dashboards" : "Cards"}
+          </Heading>
+        </Header>
+        <Box fill>
+          {state.librarySidebarMode === "dashboards" ? (
+            <DashboardLibrary />
+          ) : (
+            <CardLibrary />
+          )}
+        </Box>
+        <Box alignSelf="stretch">
+          <LoginLogout />
+        </Box>
       </Box>
-      <Box alignSelf="stretch">
-        <LoginLogout />
-      </Box>
-    </Box>
+    </Layer>
   );
 }
