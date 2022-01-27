d5cd5ae838b675123311cf920dda5249cf387423
Evan Bailey <cynicaloptimist@gmail.com>
2022-01-26 23:17:40 -0800

    Use smaller set of uppercase letters

--- src/randomString.ts
+++ src/randomString.ts
@@ -1,5 +1,5 @@
 export function randomString(length: number = 8): string {
-  const validChars = "qwertyuiopasdfghjklzxcvbnm1234567890";
+  const validChars = "QWERTYUPASDFGHJKZXCVBNM1234567890";
   let str = "";
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * validChars.length);
