dbca67ab126b3959b90cd545237f181a340e356d
Evan Bailey <cynicaloptimist@gmail.com>
2022-04-01 17:58:15 -0700

    extract component

--- src/components/cards/dice/DiceCard.tsx
+++ src/components/cards/dice/DiceCard.tsx
@@ -3,7 +3,11 @@ import { Box, Button, TextInput } from "grommet";
 import { useCallback, useContext, useRef, useState } from "react";
 import { CardActions } from "../../../actions/CardActions";
 import { ReducerContext } from "../../../reducers/ReducerContext";
-import { DiceCardState, PlayerViewPermission } from "../../../state/CardState";
+import {
+  DiceCardState,
+  DiceRoll,
+  PlayerViewPermission,
+} from "../../../state/CardState";
 import { BaseCard } from "../BaseCard";
 import { useScrollTo } from "../../hooks/useScrollTo";
 import { ViewType, ViewTypeContext } from "../../ViewTypeContext";
@@ -21,7 +25,7 @@ export function DiceCard(props: { card: DiceCardState }) {
     card.playerViewPermission === PlayerViewPermission.Interact;
 
   const rollDice = useCallback(
-    (expression) => {
+    (expression: string) => {
       const result = dice.roll(expression);
       dispatch(
         CardActions.RollDiceExpression({
@@ -43,9 +47,6 @@ export function DiceCard(props: { card: DiceCardState }) {
 
   const cardHistory = card.history || [];
   const scrollBottom = useScrollTo(cardHistory);
-
-  const diceInputRef = useRef<HTMLInputElement>(null);
-  const [lookback, setLookback] = useState(0);
 
   return (
     <BaseCard
@@ -70,33 +71,46 @@ export function DiceCard(props: { card: DiceCardState }) {
         <div ref={scrollBottom} />
       </Box>
       {canEdit && (
-        <TextInput
-          ref={diceInputRef}
-          onKeyDown={(e) => {
-            if (!diceInputRef.current) {
-              return;
-            }
-            const input = diceInputRef.current;
-            if (e.key === "Enter" && input.value.length > 0) {
-              rollDice(input.value);
-              input.value = "";
-              setLookback(0);
-            }
-            if (e.key === "ArrowUp" && lookback < cardHistory.length) {
-              input.value =
-                cardHistory[cardHistory.length - (lookback + 1)].expression;
-
-              setLookback(lookback + 1);
-            }
-            if (e.key === "ArrowDown" && lookback - 1 > 0) {
-              input.value =
-                cardHistory[cardHistory.length - (lookback - 1)].expression;
-
-              setLookback(lookback - 1);
-            }
-          }}
-        />
+        <DiceTextInput rollDice={rollDice} cardHistory={cardHistory} />
       )}
     </BaseCard>
+  );
+}
+
+function DiceTextInput(props: {
+  rollDice: (expression: string) => void;
+  cardHistory: DiceRoll[];
+}) {
+  const { rollDice, cardHistory } = props;
+  const [lookback, setLookback] = useState(0);
+  const diceInputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <TextInput
+      ref={diceInputRef}
+      onKeyDown={(e) => {
+        if (!diceInputRef.current) {
+          return;
+        }
+        const input = diceInputRef.current;
+        if (e.key === "Enter" && input.value.length > 0) {
+          rollDice(input.value);
+          input.value = "";
+          setLookback(0);
+        }
+        if (e.key === "ArrowUp" && lookback < cardHistory.length) {
+          input.value =
+            cardHistory[cardHistory.length - (lookback + 1)].expression;
+
+          setLookback(lookback + 1);
+        }
+        if (e.key === "ArrowDown" && lookback - 1 > 0) {
+          input.value =
+            cardHistory[cardHistory.length - (lookback - 1)].expression;
+
+          setLookback(lookback - 1);
+        }
+      }}
+    />
   );
 }
