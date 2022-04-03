44907da4d8661ba2ac6d60c29679764915577e4b
Evan Bailey <cynicaloptimist@gmail.com>
2022-04-02 20:19:45 -0700

    formatting

--- src/state/CardState.ts
+++ src/state/CardState.ts
@@ -45,7 +45,7 @@ export type ImageCardState = BaseCardState & {
 export type DiceCardState = BaseCardState & {
   type: "dice";
   history?: DiceRoll[];
-  quickRolls?: string [];
+  quickRolls?: string[];
 };
 
 export type DiceRoll = {
