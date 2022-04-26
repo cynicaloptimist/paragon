a03dace9432df2abbe4d0a9ce9d410c4e7a78b05
Evan Bailey <cynicaloptimist@gmail.com>
2022-04-26 00:24:53 -0700

    Basic Excalidraw card with sceneelement saving

--- src/components/cards/DrawingCard.tsx
+++ src/components/cards/DrawingCard.tsx
@@ -1,180 +1,47 @@
-import {
-  faArrowsAlt,
-  faMousePointer,
-  faPen,
-  faTint,
-} from "@fortawesome/free-solid-svg-icons";
-import { faCircle, faSquare } from "@fortawesome/free-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Box, Button, Drop } from "grommet";
+import { Box } from "grommet";
 import _ from "lodash";
-import React, { useContext, useRef, useState } from "react";
-import { SketchPicker } from "react-color";
+import React, { useContext } from "react";
+import Excalidraw from "@excalidraw/excalidraw";
 
 import { CardActions } from "../../actions/CardActions";
 import { ReducerContext } from "../../reducers/ReducerContext";
-import {
-  DrawingCardState,
-  PlayerViewPermission,
-  SketchModelJSON,
-} from "../../state/CardState";
+import { DrawingCardState, PlayerViewPermission } from "../../state/CardState";
 import { BaseCard } from "./base/BaseCard";
-import { SketchFieldProps, SketchModel, ToolsEnum } from "./SketchFieldProps";
 import { ViewTypeContext, ViewType } from "../ViewTypeContext";
+import { ExcalidrawElement } from "@excalidraw/excalidraw/types/element/types";
 
 type Size = { height: number; width: number };
-
-const {
-  Tools,
-  SketchField,
-}: {
-  Tools: ToolsEnum;
-  SketchField: React.FC<SketchFieldProps>;
-} = require("react-sketch2");
 
 export function DrawingCard(props: {
   card: DrawingCardState;
   outerSize: Size;
 }) {
   const { dispatch } = React.useContext(ReducerContext);
-  const [activeTool, setActiveTool] = useState<string>(Tools.Pencil);
-  const [color, setColor] = useState<string>("#000000");
 
-  const boxRef = React.useRef<HTMLDivElement>(null);
-  const [innerSize, setInnerSize] = React.useState<Size>({
-    height: 0,
-    width: 0,
-  });
-  const sketch = useRef<any>(null);
   const viewType = useContext(ViewTypeContext);
-
-  React.useEffect(() => {
-    if (!boxRef.current) {
-      return;
-    }
-    const box = boxRef.current;
-    setInnerSize(box.getBoundingClientRect());
-  }, [boxRef, props.outerSize]);
 
   const canEdit =
     viewType !== ViewType.Player ||
     props.card.playerViewPermission === PlayerViewPermission.Interact;
 
-  const onSketchChange = () => {
-    if (!sketch.current) {
-      return;
-    }
-
-    const newSketchModel: SketchModel = sketch.current.toJSON();
-
-    // The sketch objects must be stringified to preserve null values in Firebase.
-    const newSketchJSON: SketchModelJSON = {
-      ...newSketchModel,
-      objectJSONs: newSketchModel.objects.map((object: any) =>
-        JSON.stringify(object)
-      ),
-    };
-
-    if (!_.isEqual(newSketchJSON, props.card.sketchModel)) {
-      dispatch(
-        CardActions.SetSketchModel({
-          cardId: props.card.cardId,
-          sketchJSON: newSketchJSON,
-        })
-      );
-    }
-  };
-
-  const sketchModel = {
-    ...props.card.sketchModel,
-    objects: (props.card.sketchModel?.objectJSONs || []).map(
-      (object) => JSON.parse(object) || []
-    ),
-  };
-
-  const tools = [
-    { name: Tools.Pan, icon: faArrowsAlt },
-    {
-      name: Tools.Select,
-      icon: faMousePointer,
-    },
-    { name: Tools.Pencil, icon: faPen },
-    { name: Tools.Rectangle, icon: faSquare },
-    { name: Tools.Circle, icon: faCircle },
-  ];
-
   return (
-    <BaseCard
-      cardState={props.card}
-      commands={
-        <>
-          <ColorPickerButton color={color} setColor={setColor} />
-          {tools.map((tool) => (
-            <Button
-              key={tool.name}
-              onClick={() => setActiveTool(tool.name)}
-              icon={<FontAwesomeIcon icon={tool.icon} />}
-              active={activeTool === tool.name}
-            />
-          ))}
-        </>
-      }
-    >
-      <Box
-        fill
-        ref={boxRef}
-        tabIndex={0}
-        onKeyDown={(keyEvent) => {
-          if (keyEvent.key === "Delete") {
-            sketch.current?.removeSelected();
-          }
-          onSketchChange();
-        }}
-        onMouseUp={() => setImmediate(() => onSketchChange())}
-      >
-        <SketchField
-          tool={canEdit ? activeTool : Tools.Pan}
-          lineColor={color}
-          value={sketchModel}
-          ref={sketch}
-          width={innerSize.width}
-          height={innerSize.height}
+    <BaseCard cardState={props.card} commands={[]}>
+      <Box fill tabIndex={0}>
+        <Excalidraw
+          viewModeEnabled={!canEdit}
+          initialData={{ elements: props.card.sceneElements }}
+          onChange={(elements) => {
+            if (!_.isEqual(elements, props.card.sceneElements)) {
+              dispatch(
+                CardActions.SetSceneElements({
+                  cardId: props.card.cardId,
+                  sceneElements: elements as ExcalidrawElement[],
+                })
+              );
+            }
+          }}
         />
       </Box>
     </BaseCard>
-  );
-}
-
-function ColorPickerButton(props: {
-  color: string;
-  setColor: (color: string) => void;
-}) {
-  const buttonRef = React.useRef(null);
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  return (
-    <>
-      {isOpen && buttonRef.current && (
-        <Drop
-          target={buttonRef.current}
-          align={{ bottom: "top", right: "right" }}
-          onClickOutside={() => setIsOpen(false)}
-        >
-          <SketchPicker
-            color={props.color}
-            onChange={(colorResult) => {
-              props.setColor(colorResult.hex);
-            }}
-          />
-        </Drop>
-      )}
-      <Button
-        ref={buttonRef}
-        active={isOpen}
-        key="colorPicker"
-        onClick={() => setIsOpen(!isOpen)}
-        icon={<FontAwesomeIcon color={props.color} icon={faTint} />}
-      />
-    </>
   );
 }
