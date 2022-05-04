96817554a2980eeddd4d76ff77d8eff09cfe9298
Evan Bailey <cynicaloptimist@gmail.com>
2022-05-03 22:08:36 -0700

    DetailRow component with detail save

--- src/components/cards/clock/VerticalClock.tsx
+++ src/components/cards/clock/VerticalClock.tsx
@@ -2,7 +2,10 @@ import { faMinusSquare } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Box, Button, TextArea } from "grommet";
 import * as React from "react";
+import { CardActions } from "../../../actions/CardActions";
+import { ReducerContext } from "../../../reducers/ReducerContext";
 import { ClockCardState, PlayerViewPermission } from "../../../state/CardState";
+import { useThrottledTrailing } from "../../hooks/useThrottled";
 import { ViewType, ViewTypeContext } from "../../ViewTypeContext";
 import { useOnClickSegment } from "./useOnClickSegment";
 
@@ -17,21 +20,15 @@ export function VerticalClock(props: { card: ClockCardState }) {
   const onClickSegment = useOnClickSegment(props);
   let segments = [];
   for (let i = 0; i < props.card.max; i++) {
-    const color = i < props.card.value ? "brand" : "light-6";
     segments.push(
-      <Box direction="row" flex="grow">
-        <Box
-          key={i}
-          width={VERTICAL_CLOCK_SEGMENT_WIDTH}
-          flex={false}
-          hoverIndicator={canEdit && { color: "brand-2" }}
-          background={color}
-          onClick={canEdit ? () => onClickSegment(i) : undefined}
-        />
-        <TextArea value={props.card.details?.[i]} />
-      </Box>
+      <DetailRow
+        key={props.card.cardId + "_" + i}
+        detailIndex={i}
+        card={props.card}
+      />
     );
   }
+
   return (
     <Box
       direction="column"
@@ -52,6 +49,52 @@ export function VerticalClock(props: { card: ClockCardState }) {
       <Box direction="column" fill gap="xxsmall" justify="stretch">
         {segments}
       </Box>
+    </Box>
+  );
+}
+
+function DetailRow(props: { detailIndex: number; card: ClockCardState }) {
+  const { dispatch } = React.useContext(ReducerContext);
+  const viewType = React.useContext(ViewTypeContext);
+  const canEdit =
+    viewType !== ViewType.Player ||
+    props.card.playerViewPermission === PlayerViewPermission.Interact;
+  const onClickSegment = useOnClickSegment(props);
+
+  const onDetailSave = React.useCallback(
+    (changeEvent: React.ChangeEvent<HTMLTextAreaElement>) => {
+      dispatch(
+        CardActions.SetClockDetail({
+          cardId: props.card.cardId,
+          detailIndex: props.detailIndex,
+          detail: changeEvent.target.value,
+        })
+      );
+    },
+    [dispatch, props.card.cardId, props.detailIndex]
+  );
+  const onDetailSaveThrottled = useThrottledTrailing(onDetailSave, 200);
+
+  const i = props.detailIndex;
+  const color = i < props.card.value ? "brand" : "light-6";
+
+  return (
+    <Box direction="row" flex="grow">
+      <Box
+        key={i}
+        width={VERTICAL_CLOCK_SEGMENT_WIDTH}
+        flex={false}
+        hoverIndicator={canEdit && { color: "brand-2" }}
+        background={color}
+        onClick={canEdit ? () => onClickSegment(i) : undefined}
+      />
+      <TextArea
+        defaultValue={props.card.details?.[i]}
+        onChange={(changeEvent) => {
+          changeEvent.persist();
+          return onDetailSaveThrottled(changeEvent);
+        }}
+      />
     </Box>
   );
 }
