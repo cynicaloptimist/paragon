b2eccf191d2d608aed86fc6bb35fdecb2c5e99f0
Evan Bailey <cynicaloptimist@gmail.com>
2022-09-13 22:33:34 -0700

    grow iframe

--- src/components/cards/FrameCard.tsx
+++ src/components/cards/FrameCard.tsx
@@ -1,10 +1,15 @@
+import styled from "styled-components";
 import { FrameCardState } from "../../state/CardState";
 import { BaseCard } from "./base/BaseCard";
 
 export function FrameCard(props: { card: FrameCardState }) {
   return (
     <BaseCard cardState={props.card} commands={[]}>
-      <iframe title={props.card.title} src={props.card.frameUrl} />
+      <Frame title={props.card.title} src={props.card.frameUrl} />
     </BaseCard>
   );
 }
+
+const Frame = styled.iframe`
+  flex-grow: 1;
+`;
