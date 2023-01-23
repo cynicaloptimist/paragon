cb695a93a458e4fb0a9e5510ad1664d3ce74a8c6
Evan Bailey <cynicaloptimist@gmail.com>
2023-01-22 18:54:21 -0800

    Use spring for Rolltable scrolling

--- src/components/cards/roll-table/RollTable.tsx
+++ src/components/cards/roll-table/RollTable.tsx
@@ -1,26 +1,26 @@
 import { Box } from "grommet";
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { RollTableModel } from "./GetRollTableModel";
+import { animated, useSpring } from "@react-spring/web";
 
 export function RollTable(props: { rollTableModel: RollTableModel }) {
+  const [springs, springApi] = useSpring(() => ({}));
+
   const rolledElement = useRef<HTMLDivElement>(null);
+  const scrollContainer = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
-    if (!rolledElement.current) {
+    if (!(rolledElement.current && scrollContainer.current)) {
       return;
     }
-    const scrollTop = rolledElement.current.scrollTop;
+    const scrollDifference =
+      rolledElement.current.offsetTop - scrollContainer.current.scrollTop;
     rolledElement.current.scrollIntoView({
-      behavior: "smooth",
       block: "center",
     });
 
-    // Workaround for chromium bug https://bugs.chromium.org/p/chromium/issues/detail?id=833617
-    setTimeout(() => {
-      if (rolledElement.current?.scrollTop === scrollTop) {
-        rolledElement.current.scrollIntoView({ block: "center" });
-      }
-    }, 500);
-  }, [rolledElement, props.rollTableModel.rollResult]);
+    springApi.start({ from: { y: scrollDifference }, to: { y: 0 } });
+  }, [rolledElement, props.rollTableModel.rollResult, springApi]);
 
   return (
     <Box>
@@ -35,24 +35,28 @@ export function RollTable(props: { rollTableModel: RollTableModel }) {
         </Box>
         <Box>Result</Box>
       </Box>
-      <Box overflow="auto">
-        {props.rollTableModel.entries.map((entry, index) => {
-          return (
-            <Box
-              key={index}
-              direction="row"
-              flex="grow"
-              background={entry.isRolled ? "brand-2" : "background"}
-              ref={entry.isRolled ? rolledElement : undefined}
-            >
-              <Box flex={false} width="xsmall" align="center">
-                {entry.diceRange}
+      <Box overflow="auto" ref={scrollContainer}>
+        <SpinBox style={springs}>
+          {props.rollTableModel.entries.map((entry, index) => {
+            return (
+              <Box
+                key={index}
+                direction="row"
+                flex="grow"
+                background={entry.isRolled ? "brand-2" : "background"}
+                ref={entry.isRolled ? rolledElement : undefined}
+              >
+                <Box flex={false} width="xsmall" align="center">
+                  {entry.diceRange}
+                </Box>
+                <Box>{entry.content}</Box>
               </Box>
-              <Box>{entry.content}</Box>
-            </Box>
-          );
-        })}
+            );
+          })}
+        </SpinBox>
       </Box>
     </Box>
   );
 }
+
+const SpinBox = animated(Box);
