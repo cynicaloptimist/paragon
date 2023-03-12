b9c2f6f0909ce669563b5cda64e13e05c9337bfc
Evan Bailey <cynicaloptimist@gmail.com>
2023-03-12 01:23:18 -0800

    add id and title properties

--- src/state/CampaignState.ts
+++ src/state/CampaignState.ts
@@ -2,6 +2,8 @@ import { CardsState } from "./CardState";
 import { DashboardState } from "./DashboardState";
 
 export type CampaignState = {
+  id: string;
+  title: string;
   cardsById: CardsState;
   dashboardsById: Record<string, DashboardState>;
 };
