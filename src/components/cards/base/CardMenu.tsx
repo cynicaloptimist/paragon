c1abe0a1856c3f02c17be628e64bc7f6b378c11b
Evan Bailey <cynicaloptimist@gmail.com>
2023-09-23 14:50:46 -0700

    Card menu campaign chooser

--- src/components/cards/base/CardMenu.tsx
+++ src/components/cards/base/CardMenu.tsx
@@ -1,13 +1,48 @@
 import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Menu } from "grommet";
+import { Box, Button, Drop, Menu, Text } from "grommet";
 import React from "react";
 import { ReducerContext } from "../../../reducers/ReducerContext";
 import { randomString } from "../../../randomString";
 import { Actions } from "../../../actions/Actions";
+import { CampaignState } from "../../../state/CampaignState";
 
 export function CardMenu(props: { cardId: string }) {
-  const { dispatch } = React.useContext(ReducerContext);
+  const { state, dispatch } = React.useContext(ReducerContext);
+  const cardState = state.cardsById[props.cardId];
+  const [campaignChooserActive, setCampaignChooserActive] =
+    React.useState(false);
+  const targetRef = React.useRef(null);
+
+  if (campaignChooserActive) {
+    const campaigns: Partial<CampaignState>[] = [
+      {
+        id: undefined,
+        title: "(default campaign)",
+      },
+      ...Object.values(state.campaignsById),
+    ];
+    return (
+      <>
+        <Button ref={targetRef} icon={<FontAwesomeIcon icon={faEllipsisV} />} />
+        <Drop
+          target={targetRef}
+          align={{ right: "right", top: "bottom" }}
+          onClickOutside={() => setCampaignChooserActive(false)}
+        >
+          <ChooseCampaignText />
+          {campaigns.map((c) => (
+            <Button
+              style={{ border: "none" }}
+              label={c.title}
+              onClick={() => c.id}
+              active={cardState.campaignId === c.id}
+            />
+          ))}
+        </Drop>
+      </>
+    );
+  }
 
   return (
     <Menu
@@ -26,7 +61,19 @@ export function CardMenu(props: { cardId: string }) {
             );
           },
         },
+        {
+          label: "Change Campaign",
+          onClick: () => setCampaignChooserActive(true),
+        },
       ]}
     />
+  );
+}
+
+function ChooseCampaignText() {
+  return (
+    <Text alignSelf="center" margin="small" style={{ fontStyle: "italic" }}>
+      Choose Campaign
+    </Text>
   );
 }
