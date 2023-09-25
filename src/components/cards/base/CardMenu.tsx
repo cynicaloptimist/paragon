c927146d8cfb35495abc8da8ec7c661f0909d8b7
Evan Bailey <cynicaloptimist@gmail.com>
2023-09-24 21:10:54 -0700

    Move component to file

--- src/components/cards/base/CardMenu.tsx
+++ src/components/cards/base/CardMenu.tsx
@@ -1,12 +1,12 @@
 import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Button, Drop, Menu, Text } from "grommet";
+import { Menu, Text } from "grommet";
 import React from "react";
 import { ReducerContext } from "../../../reducers/ReducerContext";
 import { randomString } from "../../../randomString";
 import { Actions } from "../../../actions/Actions";
-import { CampaignState } from "../../../state/CampaignState";
 import { CardActions } from "../../../actions/CardActions";
+import { CampaignChooser } from "../../common/CampaignChooser";
 
 export function CardMenu(props: { cardId: string }) {
   const { state, dispatch } = React.useContext(ReducerContext);
@@ -57,45 +57,7 @@ export function CardMenu(props: { cardId: string }) {
   );
 }
 
-function CampaignChooser(props: {
-  close: () => void;
-  activeCampaignId: string | undefined;
-  selectCampaign: (campaignId: string | undefined) => void;
-}) {
-  const { state } = React.useContext(ReducerContext);
-  const targetRef = React.useRef(null);
-
-  const campaigns: Partial<CampaignState>[] = [
-    {
-      id: undefined,
-      title: "(default campaign)",
-    },
-    ...Object.values(state.campaignsById),
-  ];
-  return (
-    <>
-      <Button ref={targetRef} icon={<FontAwesomeIcon icon={faEllipsisV} />} />
-      <Drop
-        target={targetRef}
-        align={{ right: "right", top: "bottom" }}
-        onClickOutside={props.close}
-        pad="xsmall"
-      >
-        <ChooseCampaignText />
-        {campaigns.map((c) => (
-          <Button
-            style={{ border: "none" }}
-            label={c.title}
-            onClick={() => props.selectCampaign(c.id)}
-            active={props.activeCampaignId === c.id}
-          />
-        ))}
-      </Drop>
-    </>
-  );
-}
-
-function ChooseCampaignText() {
+export function ChooseCampaignText() {
   return (
     <Text alignSelf="center" margin="small" style={{ fontStyle: "italic" }}>
       Choose Campaign
---
+++ src/components/common/CampaignChooser.tsx
@@ -0,0 +1,45 @@
+import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button, Drop } from "grommet";
+import React from "react";
+import { ReducerContext } from "../../reducers/ReducerContext";
+import { CampaignState } from "../../state/CampaignState";
+import { ChooseCampaignText } from "../cards/base/CardMenu";
+
+export function CampaignChooser(props: {
+  close: () => void;
+  activeCampaignId: string | undefined;
+  selectCampaign: (campaignId: string | undefined) => void;
+}) {
+  const { state } = React.useContext(ReducerContext);
+  const targetRef = React.useRef(null);
+
+  const campaigns: Partial<CampaignState>[] = [
+    {
+      id: undefined,
+      title: "(default campaign)",
+    },
+    ...Object.values(state.campaignsById),
+  ];
+  return (
+    <>
+      <Button ref={targetRef} icon={<FontAwesomeIcon icon={faEllipsisV} />} />
+      <Drop
+        target={targetRef}
+        align={{ right: "right", top: "bottom" }}
+        onClickOutside={props.close}
+        pad="xsmall"
+      >
+        <ChooseCampaignText />
+        {campaigns.map((c) => (
+          <Button
+            style={{ border: "none" }}
+            label={c.title}
+            onClick={() => props.selectCampaign(c.id)}
+            active={props.activeCampaignId === c.id}
+          />
+        ))}
+      </Drop>
+    </>
+  );
+}
