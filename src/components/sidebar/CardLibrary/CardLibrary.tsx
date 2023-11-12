66d209e6de9c72eb8bc9b66d80e808a6cb7e3a46
Evan Bailey <cynicaloptimist@gmail.com>
2023-11-11 22:18:35 -0800

    Move const to file

--- src/components/sidebar/CardLibrary/CardLibrary.tsx
+++ src/components/sidebar/CardLibrary/CardLibrary.tsx
@@ -3,7 +3,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Fuse from "fuse.js";
 import {
   Accordion,
-  AccordionPanel,
   Box,
   BoxExtendedProps,
   Button,
@@ -14,98 +13,10 @@ import {
 } from "grommet";
 import React, { useContext, useMemo, useState } from "react";
 import { ReducerContext } from "../../../reducers/ReducerContext";
-import { AppState, isDefined } from "../../../state/AppState";
+import { isDefined } from "../../../state/AppState";
 import { CardState } from "../../../state/CardState";
-import { CardTypeFriendlyNames } from "../../../state/CardTypes";
 import { CardLibraryRow } from "./CardLibraryRow";
-
-type CardGrouping = {
-  Name: string;
-  GetGroupsForCard: (cardState: CardState, appState: AppState) => string[];
-  GetSection: React.FunctionComponent<{
-    groupName: string;
-    cards: CardState[];
-  }>;
-};
-
-const accordionBoxProps: BoxExtendedProps = {
-  gap: "xsmall",
-};
-
-export const CardGroupings: CardGrouping[] = [
-  {
-    Name: "Card Type",
-    GetGroupsForCard: (cardState: CardState) => [
-      CardTypeFriendlyNames[cardState.type],
-    ],
-    GetSection: (props: { groupName: string; cards: CardState[] }) => {
-      return (
-        <AccordionPanel label={props.groupName}>
-          <Box {...accordionBoxProps}>
-            {props.cards.map((card) => (
-              <CardLibraryRow key={card.cardId} card={card} />
-            ))}
-          </Box>
-        </AccordionPanel>
-      );
-    },
-  },
-  {
-    Name: "Dashboard",
-    GetGroupsForCard: (cardState: CardState, appState: AppState) => {
-      const dashboardIds = Object.keys(appState.dashboardsById).filter(
-        (dashboardId) => {
-          return appState.dashboardsById[dashboardId]?.openCardIds?.includes(
-            cardState.cardId
-          );
-        }
-      );
-      return dashboardIds;
-    },
-    GetSection: (props: { groupName: string; cards: CardState[] }) => {
-      const { state } = useContext(ReducerContext);
-      return (
-        <AccordionPanel label={state.dashboardsById[props.groupName]?.name}>
-          <Box {...accordionBoxProps}>
-            {props.cards.map((card) => (
-              <CardLibraryRow
-                key={props.groupName + "_" + card.cardId}
-                card={card}
-              />
-            ))}
-          </Box>
-        </AccordionPanel>
-      );
-    },
-  },
-  {
-    Name: "Folder",
-    GetGroupsForCard: (cardState: CardState) => [cardState.path ?? ""],
-    GetSection: (props: { groupName: string; cards: CardState[] }) => {
-      if (props.groupName === "") {
-        return (
-          <AccordionPanel label="(no folder)">
-            <Box {...accordionBoxProps}>
-              {props.cards.map((card) => (
-                <CardLibraryRow showFolder key={card.cardId} card={card} />
-              ))}
-            </Box>
-          </AccordionPanel>
-        );
-      }
-
-      return (
-        <AccordionPanel label={props.groupName}>
-          <Box {...accordionBoxProps}>
-            {props.cards.map((card) => (
-              <CardLibraryRow showFolder key={card.cardId} card={card} />
-            ))}
-          </Box>
-        </AccordionPanel>
-      );
-    },
-  },
-];
+import { CardGroupings } from "./CardGroupings";
 
 export function CardLibrary() {
   const { state } = useContext(ReducerContext);
