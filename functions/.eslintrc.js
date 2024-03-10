89cea3021be3d86f6db7fdd5acb74c4854f617e0
Evan Bailey <cynicaloptimist@gmail.com>
2024-03-09 17:29:51 -0800

    Fix ESLint complaint

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -12,13 +12,9 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: "tsconfig.json",
     sourceType: "module",
   },
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  plugins: ["@typescript-eslint", "import"],
   rules: {
     "@typescript-eslint/adjacent-overload-signatures": "error",
     "@typescript-eslint/no-empty-function": "error",
