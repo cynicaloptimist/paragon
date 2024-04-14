ad40096eec45de4a1d8e878e75632c05c6d4c66e
Evan Bailey <cynicaloptimist@gmail.com>
2024-04-13 20:25:34 -0700

    Supply parserOptions.project

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
     sourceType: "module",
+    project: "./package.json",
   },
   plugins: ["@typescript-eslint", "import"],
   rules: {
