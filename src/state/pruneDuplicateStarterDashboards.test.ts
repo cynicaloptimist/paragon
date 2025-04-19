982233c515bd7e9903c014bee74af457b6747f83
Evan Bailey <cynicaloptimist@gmail.com>
2025-04-18 22:59:02 -0700

    More tests

--- src/state/pruneDuplicateStarterDashboards.test.ts
+++ src/state/pruneDuplicateStarterDashboards.test.ts
@@ -1,7 +1,7 @@
-import { last } from "lodash";
 import { EmptyState } from "./AppState";
 import { DashboardState } from "./DashboardState";
 import { pruneDuplicateStarterDashboards } from "./pruneDuplicateStarterDashboards";
+import { CardState } from "./CardState";
 
 function EmptyDashboard(name: string = ""): DashboardState {
   return {
@@ -14,7 +14,7 @@ function EmptyDashboard(name: string = ""): DashboardState {
 }
 
 describe("Prune Duplicate Starter Dashboards", () => {
-  test("Should have no effect if no duplicates", () => {
+  test("Should have no effect if no starters", () => {
     const appState = EmptyState();
     appState.dashboardsById = {
       "1": EmptyDashboard("Some Dashboard"),
@@ -63,6 +63,65 @@ describe("Prune Duplicate Starter Dashboards", () => {
     expect(appState.dashboardsById).toEqual({
       "1": EmptyDashboard("Some Dashboard"),
       "3": newStarterDashboard,
+    });
+  });
+
+  test("Should not remove starter dashboards with non-info cards", () => {
+    const appState = EmptyState();
+    const oldStarterDashboard = {
+      ...EmptyDashboard("Dashboard 1"),
+      lastOpenedTimeMs: 1000,
+      openCardIds: ["1", "2"],
+    };
+    const newStarterDashboard = {
+      ...EmptyDashboard("Dashboard 1"),
+      lastOpenedTimeMs: 2000,
+      openCardIds: ["1", "2"],
+    };
+    appState.cardsById = {
+      "1": { type: "info" } as CardState,
+      "2": { type: "article" } as CardState,
+    };
+    appState.dashboardsById = {
+      "1": EmptyDashboard("Some Dashboard"),
+      "2": oldStarterDashboard,
+      "3": newStarterDashboard,
+    };
+
+    pruneDuplicateStarterDashboards(appState);
+    expect(appState.dashboardsById).toEqual({
+      "1": EmptyDashboard("Some Dashboard"),
+      "2": oldStarterDashboard,
+      "3": newStarterDashboard,
+    });
+  });
+
+  test("Should prioritize removing empty starter dashboards", () => {
+    const appState = EmptyState();
+    const oldStarterDashboard = {
+      ...EmptyDashboard("Dashboard 1"),
+      lastOpenedTimeMs: 1000,
+      openCardIds: ["1", "2"],
+    };
+    const newStarterDashboard = {
+      ...EmptyDashboard("Dashboard 1"),
+      lastOpenedTimeMs: 2000,
+      openCardIds: ["1"],
+    };
+    appState.cardsById = {
+      "1": { type: "info" } as CardState,
+      "2": { type: "article" } as CardState,
+    };
+    appState.dashboardsById = {
+      "1": EmptyDashboard("Some Dashboard"),
+      "2": oldStarterDashboard,
+      "3": newStarterDashboard,
+    };
+
+    pruneDuplicateStarterDashboards(appState);
+    expect(appState.dashboardsById).toEqual({
+      "1": EmptyDashboard("Some Dashboard"),
+      "2": oldStarterDashboard,
     });
   });
 });
