d12a3398629483f9e6f90b7861fd6d0583707e33
Evan Bailey <cynicaloptimist@gmail.com>
2025-06-06 22:52:54 -0700

    Use frontend-available env var

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -28,7 +28,10 @@ type Config = {
 export function register(config?: Config) {
   if (import.meta.env.PROD && "serviceWorker" in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(import.meta.env.PUBLIC_URL, window.location.href);
+    const publicUrl = new URL(
+      import.meta.env.VITE_PUBLIC_URL,
+      window.location.href
+    );
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
@@ -37,7 +40,7 @@ export function register(config?: Config) {
     }
 
     window.addEventListener("load", () => {
-      const swUrl = `${import.meta.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${import.meta.env.VITE_PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
