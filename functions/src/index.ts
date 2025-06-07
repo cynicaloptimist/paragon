09a813b843f8293020ae417ec57b45631b17326d
Evan Bailey <cynicaloptimist@gmail.com>
2025-06-06 22:54:28 -0700

    Add functions v2 login endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,13 @@
 import * as admin from "firebase-admin";
-import * as functions from "firebase-functions";
+import * as functionsv1 from "firebase-functions/v1";
+import { onRequest } from "firebase-functions/v2/https";
+import { logger } from "firebase-functions/v2";
+const { defineString } = require("firebase-functions/params");
 import * as Url from "url";
 
 import contributors from "./thanks";
 
 const patreon = require("@nathanhigh/patreon");
-
-const { patreon_config } = functions.config();
 
 const patreonAPI = patreon.patreon;
 const patreonOAuth = patreon.oauth;
@@ -23,14 +24,15 @@ const tiersWithEpicEntitled = ["1937132", "8749940"];
 
 admin.initializeApp();
 
-const patreonOAuthClient = patreonOAuth(
-  patreon_config.client_id,
-  patreon_config.client_secret
-);
+const client_id = defineString("PATREON_CONFIG_CLIENT_ID").value();
+const client_secret = defineString("PATREON_CONFIG_CLIENT_SECRET").value();
+const redirect_url = defineString("PATREON_CONFIG_REDIRECT_URL").value();
+
+const patreonOAuthClient = patreonOAuth(client_id, client_secret);
 
 type ApiListing = { type: string; id: string };
 
-export const patreon_login = functions.https.onRequest(
+export const patreon_login = functionsv1.https.onRequest(
   async (request, response) => {
     if (!request.query) {
       console.warn("Login redirect called with no query parameters");
@@ -38,13 +40,13 @@ export const patreon_login = functions.https.onRequest(
       return;
     }
 
-    functions.logger.info("Patreon Login Redirect: ", request.query);
+    functionsv1.logger.info("Patreon Login Redirect: ", request.query);
 
     const oauthGrantCode = Url.parse(request.url, true).query.code;
     try {
       const tokensResponse = await patreonOAuthClient.getTokens(
         oauthGrantCode,
-        patreon_config.redirect_url
+        redirect_url
       );
 
       const patreonAPIClient = patreonAPI(tokensResponse.access_token);
@@ -52,7 +54,7 @@ export const patreon_login = functions.https.onRequest(
         "/identity?include=memberships,memberships.currently_entitled_tiers"
       );
       const userIdentity = await patreonAPIClient(url);
-      functions.logger.info("User: ", JSON.stringify(userIdentity.rawJson));
+      functionsv1.logger.info("User: ", JSON.stringify(userIdentity.rawJson));
 
       const patreonId = userIdentity.rawJson.data?.id;
       const entitledTiers: string[] =
@@ -60,11 +62,11 @@ export const patreon_login = functions.https.onRequest(
           ?.filter((include: ApiListing) => include.type === "tier")
           .map((include: ApiListing) => include.id) || [];
 
-      functions.logger.info("Entitled Tier Ids: ", entitledTiers);
+      functionsv1.logger.info("Entitled Tier Ids: ", entitledTiers);
 
       const isContributor = contributors.some((c) => c.PatreonId === patreonId);
 
-      functions.logger.info("isContributor: ", isContributor);
+      functionsv1.logger.info("isContributor: ", isContributor);
 
       const hasStorage =
         isContributor ||
@@ -88,7 +90,72 @@ export const patreon_login = functions.https.onRequest(
 
       response.redirect(redirectUrl.toString());
     } catch (err) {
-      functions.logger.error("error!", err);
+      functionsv1.logger.error("error!", err);
+      response.status(500).send(err);
+    }
+  }
+);
+
+export const patreon_login_v2 = onRequest(
+  { cors: true },
+  async (request, response) => {
+    if (!request.query) {
+      console.warn("Login redirect called with no query parameters");
+      response.sendStatus(400);
+      return;
+    }
+
+    logger.info("Patreon Login Redirect: ", request.query);
+
+    const oauthGrantCode = Url.parse(request.url, true).query.code;
+    try {
+      const tokensResponse = await patreonOAuthClient.getTokens(
+        oauthGrantCode,
+        redirect_url
+      );
+
+      const patreonAPIClient = patreonAPI(tokensResponse.access_token);
+      const url = jsonApiURL(
+        "/identity?include=memberships,memberships.currently_entitled_tiers"
+      );
+      const userIdentity = await patreonAPIClient(url);
+      logger.info("User: ", JSON.stringify(userIdentity.rawJson));
+
+      const patreonId = userIdentity.rawJson.data?.id;
+      const entitledTiers: string[] =
+        userIdentity.rawJson.included
+          ?.filter((include: ApiListing) => include.type === "tier")
+          .map((include: ApiListing) => include.id) || [];
+
+      logger.info("Entitled Tier Ids: ", entitledTiers);
+
+      const isContributor = contributors.some((c) => c.PatreonId === patreonId);
+
+      logger.info("isContributor: ", isContributor);
+
+      const hasStorage =
+        isContributor ||
+        entitledTiers.some((entitledTier) =>
+          tiersWithAccountSyncEntitled.includes(entitledTier)
+        );
+      const hasEpic =
+        isContributor ||
+        entitledTiers.some((entitledTier) =>
+          tiersWithEpicEntitled.includes(entitledTier)
+        );
+
+      const authToken = await admin
+        .auth()
+        .createCustomToken(patreonId, { hasStorage, hasEpic });
+
+      const state = JSON.parse(request.query?.state?.toString() || "null");
+      const redirectUrl = new URL(state.finalRedirect);
+
+      redirectUrl.searchParams.append("authToken", authToken);
+
+      response.redirect(redirectUrl.toString());
+    } catch (err) {
+      logger.error("error!", err);
       response.status(500).send(err);
     }
   }
